41354ff610be9bde771e1662030172bef99f46d8
Carlos Maroto <carlosmaroto.daweb@gmail.com>
2023-11-03 17:59:09 +0100

    v1.0

--- lib/ui/main_page.dart
+++ lib/ui/main_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:appclients/common/file.dart';
+import 'package:appclients/ui/documents_page.dart';
+import 'package:appclients/ui/news_page.dart';
+import 'package:appclients/ui/profile_page.dart';
+import 'package:appclients/ui/settings_page.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -299,7 +304,7 @@ class _MainPage extends State<MainPage> {
           ),
           SizedBox(
             width: returnResponsiveWidth(context, 1),
-            height: returnResponsiveHeight(context, 0.45),
+            height: returnResponsiveHeight(context, 0.412),
             child: ListView.builder(
                 itemCount: recentFiles.length,
                 itemBuilder: ((context, index) {
@@ -402,6 +407,84 @@ class _MainPage extends State<MainPage> {
           )
         ]),
       ]),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        iconSize: 30,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        unselectedItemColor: Colors.white54,
+        selectedItemColor: Colors.white,
+        backgroundColor: const Color.fromRGBO(45, 43, 43, 1),
+        currentIndex: 2,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.source),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.rss_feed),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_filled),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: '',
+          ),
+        ],
+      ),
     );
+  }
+
+  void _onItemTapped(int index) {
+    switch (index) {
+      case 0:
+        handleNavigateTapToControllerDocumentsPage(context);
+        break;
+      case 1:
+        handleNavigateTapToControllerNewsPage(context);
+        break;
+      case 2:
+        handleNavigateTapToControllerMainPage(context);
+        break;
+      case 3:
+        handleNavigateTapToControllerProfilePage(context);
+        break;
+      case 4:
+        handleNavigateTapToControllerSettingsPage(context);
+        break;
+    }
+  }
+
+  void handleNavigateTapToControllerDocumentsPage(BuildContext context) {
+    Navigator.of(context)
+        .push(CupertinoPageRoute(builder: (_) => const DocumentsPage()));
+  }
+
+  void handleNavigateTapToControllerNewsPage(BuildContext context) {
+    Navigator.of(context)
+        .push(CupertinoPageRoute(builder: (_) => const NewsPage()));
+  }
+
+  void handleNavigateTapToControllerMainPage(BuildContext context) {
+    Navigator.of(context)
+        .push(CupertinoPageRoute(builder: (_) => const MainPage()));
+  }
+
+  void handleNavigateTapToControllerProfilePage(BuildContext context) {
+    Navigator.of(context)
+        .push(CupertinoPageRoute(builder: (_) => const ProfilePage()));
+  }
+
+  void handleNavigateTapToControllerSettingsPage(BuildContext context) {
+    Navigator.of(context)
+        .push(CupertinoPageRoute(builder: (_) => const SettingsPage()));
   }
 }
